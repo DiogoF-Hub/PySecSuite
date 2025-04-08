cdbf8888aa4fa82a7d84170f5684f1418b7cb39e
Cyb3rTyr <MarquesSaRodrigo@gmail.com>
2025-04-08 21:14:50 +0200

    adding the main tools

---
+++ Modules/osint_toolkit/OSINT.py
@@ -0,0 +1,25 @@
+# ================================================ Main for OSINT =============================================================
+
+import sherlock  # Personal Information & Social Media OSINT
+import requests  # Digital Footprint & Breach Monitoring  && Government, Legal & Business Records
+
+# Website & Domain Intelligence
+# git clone https://github.com/laramies/theHarvester.git
+# cd theHarvester
+# pip install -r requirements.txt
+
+import shodan  # IP & Network OSINT
+import twint  # Social Media & Geolocation Tracking
+
+# Dark Web & Cybercrime Monitoring
+# git clone https://github.com/megadose/OnionSearch.git
+# cd OnionSearch
+# pip install -r requirements.txt
+
+# Email & Phone Number OSINT
+# git clone https://github.com/sundowndev/phoneinfoga.git
+# cd phoneinfoga
+
+# ================================================ others =============================================================
+
+import os
