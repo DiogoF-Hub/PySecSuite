56c32f9f08703838f71f187e35dd2f2cf49fcbac
Cyb3rTyr <MarquesSaRodrigo@gmail.com>
2025-04-10 11:06:22 +0200

    new libraries upodated

--- Modules/osint_toolkit/OSINT.py
+++ Modules/osint_toolkit/OSINT.py
@@ -1,25 +1,15 @@
-# ================================================ Main for OSINT =============================================================
+# ================================================ Libraries =============================================================
+import os
 
 import sherlock  # Personal Information & Social Media OSINT
-import requests  # Digital Footprint & Breach Monitoring  && Government, Legal & Business Records
+import request
+from bs4 import BeautifulSoup
 
 # Website & Domain Intelligence
 # git clone https://github.com/laramies/theHarvester.git
 # cd theHarvester
 # pip install -r requirements.txt
 
-import shodan  # IP & Network OSINT
-import twint  # Social Media & Geolocation Tracking
-
-# Dark Web & Cybercrime Monitoring
-# git clone https://github.com/megadose/OnionSearch.git
-# cd OnionSearch
-# pip install -r requirements.txt
-
 # Email & Phone Number OSINT
 # git clone https://github.com/sundowndev/phoneinfoga.git
 # cd phoneinfoga
-
-# ================================================ others =============================================================
-
-import os
